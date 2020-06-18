0d10a9fc647a73dffad59b57109400b32e176f26
韩镇江 <Han1534930844@163.com>
2020-06-18 11:59:15 +0800

    new project

---
+++ LinuxCppServer/cmake-build-debug/CMakeFiles/LinuxCppServer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinuxCppServer.dir/main.cpp.obj"
+  "LinuxCppServer.exe"
+  "LinuxCppServer.exe.manifest"
+  "LinuxCppServer.pdb"
+  "libLinuxCppServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinuxCppServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LinuxCppServer/cmake-build-debug/CMakeFiles/LinuxCppServer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LinuxCppServer.
+# This may be replaced when dependencies are built.
